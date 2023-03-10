fcebff5cf3768a8412b0f6f7aef7517050c47015
Mike Beliakovski <mike.beliakovski@gmail.com>
2023-03-10 18:04:32 +0100

    Hide NASA API key into gradle properties

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "cG6F1Ppv5qVbrlFI0M0LJ96oyy82vWcUUikgshX0"
+    val API_KEY = BuildConfig.NASA_API_KEY
     const val ASTEROIDS_TABLE_NAME = "asteroids"
     const val PICTURE_TABLE_NAME = "pictures"
 }
